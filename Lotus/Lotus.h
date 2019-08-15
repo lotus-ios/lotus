882675fe30a0e654614eeba9d45b99b1f73ba6a6
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-08-15 16:53:29 +0300

    Initial Commit

---
+++ Lotus/Lotus.h
@@ -0,0 +1,19 @@
+//
+//  Lotus.h
+//  Lotus
+//
+//  Created by Vladislav Kondrashkov on 8/15/19.
+//  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lotus.
+FOUNDATION_EXPORT double LotusVersionNumber;
+
+//! Project version string for Lotus.
+FOUNDATION_EXPORT const unsigned char LotusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lotus/PublicHeader.h>
+
+
